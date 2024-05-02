a11ff1fbefe76964e632d2887a06a5843d57cbf1
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2024-05-02 13:54:35 +0530

    Added Task1 project
    Create APIs for
    
    1.1 User Registration using Email Password and username
    1.2 User Login using username and password
    1.3 Forget User password API

---
+++ server.js
@@ -0,0 +1,160 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const twilio = require("twilio")(
+  process.env.TWILIO_sid,
+  process.env.TWILIO_auth_token
+);
+const mongoose = require("mongoose");
+const User = require("./userModel");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.email",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "varunkhanijo14@gmail.com",
+    pass: "khjbtzxnyndnedfq",
+  },
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Enter Valid email address",
+      });
+    }
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const result = await User.create({
+      name: username,
+      email: email,
+      password: hashedpassword,
+    });
+    return res.status(201).json({ message: "New User Created", user: result });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+app.post("/forgotpass", async (req, res) => {
+  const { method, value } = req.body;
+  const otp = Math.floor(Math.random() * 9000) + 1000;
+  try {
+    if (!method) {
+      return res.status(400).json({
+        message: "Please provide method of recovery",
+      });
+    }
+
+    switch (method) {
+      case "email":
+        try {
+          let user = await User.findOne({ email: value });
+          if (!user) {
+            return res.status(400).json({
+              message: "No such User",
+            });
+          }
+          await transporter.sendMail({
+            from: '"App Auth" <varunkhanijo14@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: "Forgot Password", // Subject line
+            text: `Your Auth OTP is ${otp}`, // plain text body
+          });
+        } catch (err) {
+          console.log("Error in sending mail", err.message);
+        }
+      case "phone":
+        try {
+          const phoneNumber = value;
+          const text = `OTP for forgot password is ${otp}. `;
+          await twilio.messages.create({
+            from: `${process.env.TWILIO_phnumber}`,
+            to: `+91${phoneNumber}`,
+            body: text,
+          });
+        } catch (error) {
+        console.log("Error in sending otp message", error.message);
+        return res.status(400).json({message:"Error in sending otp message"})
+        }
+    }
+    return res.status(201).json({ message: "OTP Sent successfully!", otp: String(otp) });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/updatepassword", async (req, res) => {
+  const { email, newPassword } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error("Error updating password:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email: email });
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "User Not Found",
+      });
+    }
+    const matchPassword = await bcrypt.compare(password, existingUser.password);
+    if (!matchPassword) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    res
+      .status(200)
+      .json({ message: "Logged in successfully!", user: existingUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on ${port}, http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
