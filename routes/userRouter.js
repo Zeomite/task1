ce0d33be08a6ff6e66a6e5d5e78722822a1e3b18
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2024-05-19 21:34:32 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { signup, signin, updatepassword, forgotpass  } = require("../controllers/userController");
+const passport = require("passport");
+require("../passportConfig.js")(passport);
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
-userRouter.post("/signin", signin);
-userRouter.post("/forgotpass", forgotpass);
-userRouter.post("/updatepassword",updatepassword);
+userRouter.post("/signin",passport.authenticate('jwt', { session: false }), signin);
+userRouter.post("/forgotpass",passport.authenticate('jwt', { session: false }), forgotpass);
+userRouter.post("/updatepassword",passport.authenticate('jwt', { session: false }), updatepassword);
 
 
 module.exports = userRouter;
