4410e052eee6897af33a6c60ce5035c3aab6b142
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2024-05-19 21:34:51 +0530

    Update postRouter.js

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { create, update, remove, read, like, upload } = require("../controllers/postController");
-const ifPostImage = require("../middleware/contentMiddleware");
-
+const passport = require("passport");
+require("../passportConfig")(passport);
 
 const postRouter = express.Router();
 
-postRouter.post("/create", upload.single('file'), create);
-postRouter.post("/update", upload.single('file'), update);
-postRouter.post("/delete", remove);
-postRouter.post("/read", read);
-postRouter.post("/like", like)
+postRouter.post("/create",passport.authenticate('jwt', { session: false }), upload.single('file'), create);
+postRouter.post("/update",passport.authenticate('jwt', { session: false }), upload.single('file'), update);
+postRouter.post("/delete",passport.authenticate('jwt', { session: false }), remove);
+postRouter.post("/read",passport.authenticate('jwt', { session: false }), read);
+postRouter.post("/like", passport.authenticate('jwt', { session: false }),like)
 
 
 
