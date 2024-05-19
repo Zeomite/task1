14aa24bdb484e9573cbeac17d6d41e46ac134b20
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2024-05-19 21:35:10 +0530

    Update commentRouter.js

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const commentController = require('../controllers/commentController');
+const passport = require("passport");
+require("../passportConfig.js")(passport);
 
 const commentRouter = express.Router();
 
-commentRouter.post('/create', commentController.create);
-commentRouter.post('/update', commentController.update);
-commentRouter.post('/delete', commentController.remove);
+commentRouter.post('/create', passport.authenticate('jwt', { session: false }),commentController.create);
+commentRouter.post('/update',passport.authenticate('jwt', { session: false }), commentController.update);
+commentRouter.post('/delete',passport.authenticate('jwt', { session: false }), commentController.remove);
 
 module.exports = commentRouter;
